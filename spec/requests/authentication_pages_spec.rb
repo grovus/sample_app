c3975e81afa36ab931ea9a20b1096f6be9c0c92b
Sean White <seanywhite@gmail.com>
2013-12-11 11:45:24 -0500

    some mods form exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,7 +15,8 @@ describe "AuthenticationP" do
 
       describe "after visiting another page" do
       	before { click_link "Home" }
-      	it { should_not have_selector('div.alert.alert-error') }
+#      	it { should_not have_selector('div.alert.alert-error') }
+      	it { should_not have_error }
       end
     end
 
